d97528ccd98669d74aa9f7dd7aa97c714c90e725
your_name <andrewmiracle100@gmail.com>
2025-10-15 13:11:32 +0100

    add: http methods and endpoints

---
+++ main.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+posts = {
+    1: {
+        "author": "john",
+        "content": "RESTful APIs allow communication between a client and a server using standard HTTP methods such as GET, POST, PUT, and DELETE"
+    }
+}
+
+class Post(BaseModel):
+    author: str
+    content: str
+
+class UpdatePost(BaseModel):
+    author: str | None = None
+    content: str | None = None
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def read_post(post_id: int):
+    if post_id not in posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    return posts[post_id]
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    if not posts:
+        posts[1] = post
+    else:
+        post_id = len(posts) + 1
+    posts[post_id] = post
+
+    return posts[post_id]
+
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, post: Post): 
+    if post_id not in posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    posts[post_id] = post
+
+    return posts[post_id]
+
+@app.patch("/posts/{post_id}")
+def update_post_content(post_id: int, post: UpdatePost):
+    if post_id not in posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    if post.author == None:
+        post.author = posts[post_id]["author"]
+
+    if post.content == None:
+        post.content = posts[post_id]["content"]
+        
+    posts[post_id]["author"] = post.author
+    posts[post_id]["content"] = post.content
+    return {"message": "Post update successfull"}
+
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int):
+    if post_id not in posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    
+    del posts[post_id]
+
+    return 
